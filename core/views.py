86ab0b2c56dd4c5903206b7e9845a11a1a3a849b
rubikazzbassmrn <rubikazzbassmrn@gmail.com>
2020-11-23 11:16:22 -0600

    Mostramos los cursos asignados que tiene el usuario

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from user_profile.models import UserCourse
 from django.db.models import Q
+
 # Create your views here.
 
 def home(request):
@@ -8,4 +9,4 @@ def home(request):
         Q(user = request.user),
         Q(status = 1) | Q(status = 3) | Q(status = 4)
     )
-    return render(request, "core/panel.html", {'usercourse':usercourse})
+    return render(request, "core/panel.html", {'usercourse': usercourse})
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from user_profile.models import UserCourse
+from status.models import Status
+from django.db.models import Q
 
 # Create your views here.
 
 def dashboard(request):
-    return render(request, "dashboard/dashboard.html")
+    # Obtenemos nuestro ID
+    id_course = int(request.GET['usercourse'])
+    usercourse = UserCourse.objects.filter(
+        Q(id = id_course),
+        Q(status = 1) | Q(status = 3)
+    )
+    return render(request, "dashboard/dashboard.html", {'usercourse':usercourse})
