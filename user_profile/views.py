49dc517f1a85d81da24bd968a2838d7db53a68d7
jrojasmrn <jrojas@logsys.com.mx>
2020-12-04 01:52:40 -0600

    Se crea el template funcional sobre la vista de editar perfil, falta agregar funcion para actualizar datos

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 # Import UserProfile model
 from .models import UserProfile
+# Import Catalogue models
+from catalogues.models import StatesList, LanguajeList
 
 # Create your views here.
 
 def user_profile(request):
     profile = UserProfile.objects.all().filter(user=request.user)
     return render(request, "user_profile/profile.html", {'profile':profile})
+
+# Update user profile view
+def update_user_profile(request):
+    states = StatesList.objects.all()
+    languajes = LanguajeList.objects.all()
+    profile = UserProfile.objects.all().filter(user=request.user)
+    return render(request, "user_profile/update_profile.html", {'profile':profile, 'states':states, 'languajes':languajes})
